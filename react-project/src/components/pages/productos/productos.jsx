ef8552bf107374c938a075176e91f549cc15eb81
Macu Ortiz <macuortiz.c@gmail.com>
2023-05-22 15:02:36 -0500

    Primera entrega React

--- react-project/src/components/pages/productos/productos.jsx
+++ react-project/src/components/pages/productos/productos.jsx
@@ -1,7 +1,10 @@
+import { BotonComprar } from "../../common/counter/counter";
+
 export const ProductsList = () => {
   return (
     <div>
       <h2>aqui van los productos</h2>;
+      <BotonComprar />
     </div>
   );
 };
