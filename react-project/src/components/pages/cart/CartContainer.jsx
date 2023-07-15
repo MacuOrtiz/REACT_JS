047ab3547e593b08210d586160fdb0ba85fba2f4
Macu Ortiz <macuortiz.c@gmail.com>
2023-07-14 19:51:54 -0500

    Entrega final react

--- react-project/src/components/pages/cart/CartContainer.jsx
+++ react-project/src/components/pages/cart/CartContainer.jsx
@@ -1,34 +1,68 @@
 import { useContext } from "react";
 import { CartContext } from "../../../context/CartContext";
 import useCount from "../../../hooks/useCount";
+import Button from '@mui/material/Button';
 
 const CartContainer = () => {
   const { cart, clearCart, deleteProduct } = useContext(CartContext);
-  console.log(cart)
+  console.log(cart);
+
+  const getTotal = () => {
+    return cart.reduce((total, producto) => {
+      const { count } = useCount(producto.cantidad, producto.stock);
+      const subTotal = producto.price * count;
+      return total + subTotal;
+    }, 0);
+  };
 
   return (
-    <div>
-      <button onClick={clearCart}>Limpiar carrito</button>
+    <div className="container">
+      <h1 className="mb-3 text-uppercase text-lg-center">
+        urku extreme mountain tours
+      </h1>
+
       {cart.map((producto) => {
         const { count, añadir, eliminar } = useCount(
           producto.cantidad,
           producto.stock
         );
 
+        const subTotal = producto.price * count;
+
         return (
-          <div key={producto.id}>
-            <h2>{producto.title}</h2>
-            <h3>${producto.price}</h3>
-           
-            <p>Numero de Personas: {count}</p>
-            <button onClick={añadir}>+</button>
-            <button onClick={eliminar}>-</button>
-            <button onClick={() => deleteProduct(producto.id)}>
-              Eliminar Tour
-            </button>
+          <div key={producto.id} className="card mb-3">
+            <div className="card-body">
+              <h2 className="card-title">{producto.title}</h2>
+              <h3 className="card-text">${producto.price}</h3>
+              <p className="card-text">Numero de Personas: {count}</p>
+              <button className="btn btn-primary me-2" onClick={añadir}>
+                +
+              </button>
+              <button className="btn btn-primary" onClick={eliminar}>
+                -
+              </button>
+              <div>
+                <p>Subtotal: $ {subTotal}</p>
+              </div>
+              <div>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteProduct(producto.id)}
+                >
+                  Eliminar Tour
+                </button>
+              </div>
+            </div>
           </div>
         );
       })}
+
+      <div className="text-center">
+        <Button variant="contained" color="secondary" onClick={clearCart}>
+          Limpiar carrito
+        </Button>
+      </div>
+      <h2 className="text-center mt-4">Total: $ {getTotal()}</h2>
     </div>
   );
 };
